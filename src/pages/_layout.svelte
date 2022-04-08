25fd65f35bc841281566e4acf78d3f381abda21e
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-04-07 22:07:25 -0700

    Initial commit

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Nav from "./_nav.svelte";
+</script>
+
+<nav tabindex=0>
+  <Nav />
+</nav>
+
+<slot />
---
+++ src/pages/_nav.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { layout, isActive, url, goto, page } from "@sveltech/routify";
+  export let node = $layout;
+</script>
+
+
+<nav>
+  {#each node.children as node}
+    <a href={$url(node.path)} class:active={$isActive(node.path)}>
+      {node.title}
+    </a>
+    <!-- Here we nest our nav component within itself and pass the current node's children to it     
+         If you don't want nesting. Simply delete the snippet -->
+    {#if node.children.length}
+      <svelte:self {node} />
+    {/if}
+    <!--  -->
+  {/each}
+</nav>
